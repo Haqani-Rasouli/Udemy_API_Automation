b5ec660a2ad5be48617ffbef7cac2ed005b443dc
Haqani Rasouli <haqqanihaqiq@gmail.com>
2026-07-24 23:35:46 +0430

    API TESTING DAY_01

---
+++ tests/workingWithAPI.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://conduit.bondaracademy.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+});
+

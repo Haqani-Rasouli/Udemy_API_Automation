d8293d269a8a59a4a6865d7f84f36ea61c63ae48
Haqani Rasouli <haqqanihaqiq@gmail.com>
2026-07-25 23:19:40 +0430

    API Authomation

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,9 +1,18 @@
 import { test, expect } from '@playwright/test';
+import tags from '../data/tags.json';
+test.beforeEach(async ({ page }) => {
+  // Setup route interception
+  await page.route('https://conduit-api.bondaracademy.com/api/tags', async (route) => {
+    await route.fulfill({
+      body: JSON.stringify(tags),
+    });
+  });
 
-test('has title', async ({ page }) => {
+  // Navigate to the page before each test
   await page.goto('https://conduit.bondaracademy.com/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page.locator('.navbar-brand')).toHaveText('conduit');
 });
 
+test('Is the conduit text displayed correctly?', async ({ page }) => {
+  // No need to navigate again - it's already done in beforeEach
+  await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+});
